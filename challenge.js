facff3477be86d03591e9d376791ed8726939ed2
ost-le_thi_mai <le_thi_mai@ostechnology.co.jp>
2021-10-13 19:14:46 +0900

    fds

---
+++ challenge.js
@@ -0,0 +1,21 @@
+// // challenge
+// const dogStore = {
+//   kateDogs: [4, 1, 15, 8, 3],
+//   juliaDogs: [3, 5, 2, 12, 7],
+// };
+
+// // challenge 1
+// const checkDogs = function (kateDogs, juliaDogs) {
+//   const onlyDogs = juliaDogs.slice(1, -2);
+//   const mixDogs = juliaDogs.concat(onlyDogs);
+
+//   mixDogs.forEach((dog, i) => {
+//     if (dog > 3) {
+//       console.log(`Dog number ${i + 1} is an adult , and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is an baby , and is ${dog} years old`);
+//     }
+//   });
+// };
+
+// checkDogs(dogStore.kateDogs, dogStore.juliaDogs);
